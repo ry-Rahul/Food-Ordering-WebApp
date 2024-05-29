2fb522d13104c526b2b72f17c39b429f2a60e676
ry-Rahul <ryrahul12345@gmail.com>
2024-05-30 04:34:07 +0530

    waiting the page while login is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
@@ -6,15 +5,25 @@ import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 import PlaceOrder from "./pages/placeOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 function App() {
 
-  const [showLogin , setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
+
+  // Use useEffect to add/remove the no-scroll class to the body
+  useEffect(() => {
+    if (showLogin) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }, [showLogin]);
+
   return (
-    <div className={showLogin ? 'no-scroll' : ''}>
-    {showLogin ? <LoginPopup setShowLogin={setShowLogin}  /> :<> </>}
+    <div>
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
       <div className="app"> 
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
